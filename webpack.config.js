bffbb797d779ec24dec6738d43b7e3b16c98129a
Navdeep Singh <navdeep.er@gmail.com>
2020-01-31 18:01:12 +0530

    Add: css-loader added to parse css files

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
       {
         test: /\.html$/,
         use: "html-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
       }
     ]
   },
